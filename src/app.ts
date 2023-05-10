fdafb67eecbdc488473aa4131b2c8687fc658eb3
okonomiyakki <parkji12532@naver.com>
2023-05-10 23:12:49 +0900

    feat: dummyData 초기화 함수 추가함

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,13 @@
 import express, { Request, Response } from 'express';
 import { env } from './config/envconfig';
 import { db } from './config/dbconfig';
+import {
+  // insertDummyUsers,
+  insertDummyReviews,
+  insertDummyPosts,
+  insertDummyPets,
+  insertDummyComments,
+} from './database/dummyDatas';
 
 const port = Number(env.PORT);
 const app = express();
@@ -20,10 +27,26 @@ app.get('/', (req: Request, res: Response) => {
 });
 
 db.getConnection()
-  .then(() => console.log('✅ mysql2 로 DB 접속!'))
+  .then(async () => {
+    console.log('✅ mysql2 로 DB 접속!');
+
+    /*
+    DB dummyData 초기화가 꼭! 필요할때만 사용해야함
+    await insertDummyReviews();
+    await insertDummyPosts();
+    await insertDummyComments();
+    await insertDummyPets();
+    */
+
+    app.listen(port, () => {
+      console.log('PORT:', env.PORT);
+      console.log(`Server is running on port ${port}`);
+    });
+  })
   .catch((err) => console.log('error!!!!!!!', err));
 
-app.listen(port, () => {
-  console.log('PORT:', env.PORT);
-  console.log(`Server is running on port ${port}`);
-});
+// async function dummy() {
+//   const dummy = await db.query('SELECT * FROM comment');
+//   console.log(dummy[0]);
+// }
+// dummy();

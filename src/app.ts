37e7bdc6b64718f2bdd63bf962002f1ec3c55af6
subin <subin012358@gmail.com>
2023-05-10 13:42:50 +0900

    DB connect

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import { env } from './config/envconfig'
+import { db } from './config/dbconfig';
 
 const port = Number(env.PORT)
 const app = express();
@@ -17,6 +18,10 @@ const test: test = {
 app.get("/", (req: Request, res: Response) => {
     res.send(test);
 });
+
+db.getConnection()
+  .then(() => console.log("✅ mysql2 로 DB 접속!"))
+  .catch((err) => console.log("error!!!!!!!", err));
 
 app.listen(port, () => {
     console.log('PORT:', env.PORT);
--- src/config/envconfig.ts
+++ src/config/envconfig.ts
@@ -5,8 +5,11 @@ dotenv.config();
 export const env = {
     BCRYPT_SALT_ROUNDS: process.env.BCRYPT_SALT_ROUNDS,
     PORT: process.env.PORT,
-    // DB_HOST: process.env.DB_HOST,
-    // DB_NAME: process.env.DB_NAME,
+    DB_HOST: process.env.DB_HOST,
+    DB_PORT: process.env.DB_PORT,
+    DB_USERNAME: process.env.DB_USERNAME,
+    DB_PASSWORD: process.env.DB_PASSWORD,
+    DB_DBNAME: process.env.DB_DBNAME,
     // ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
     // REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET,
     // ACCESS_TOKEN_EXPIRES_IN: process.env.ACCESS_TOKEN_EXPIRES_IN,

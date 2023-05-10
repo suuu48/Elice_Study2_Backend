63c53b88f440250da8f8ccaec000583952590c2f
subin <subin012358@gmail.com>
2023-05-10 14:12:30 +0900

    prettier 적용

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,29 @@
-import express, { Request, Response } from "express";
-import { env } from './config/envconfig'
+import express, { Request, Response } from 'express';
+import { env } from './config/envconfig';
 import { db } from './config/dbconfig';
 
-const port = Number(env.PORT)
+const port = Number(env.PORT);
 const app = express();
 
 interface test {
-    title: string;
-    content: string;
+  title: string;
+  content: string;
 }
 
 const test: test = {
-    title: "peeps",
-    content: "typescript-server",
+  title: 'peeps',
+  content: 'typescript-server',
 };
 
-app.get("/", (req: Request, res: Response) => {
-    res.send(test);
+app.get('/', (req: Request, res: Response) => {
+  res.send(test);
 });
 
 db.getConnection()
-  .then(() => console.log("✅ mysql2 로 DB 접속!"))
-  .catch((err) => console.log("error!!!!!!!", err));
+  .then(() => console.log('✅ mysql2 로 DB 접속!'))
+  .catch((err) => console.log('error!!!!!!!', err));
 
 app.listen(port, () => {
-    console.log('PORT:', env.PORT);
-    console.log(`Server is running on port ${port}`);
+  console.log('PORT:', env.PORT);
+  console.log(`Server is running on port ${port}`);
 });

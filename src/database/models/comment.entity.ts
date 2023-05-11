ad6c18f72e10743ce1a9275f917a85f793de1a7a
okonomiyakki <parkji12532@naver.com>
2023-05-12 03:23:50 +0900

    feat: Comment Entity 생성

---
+++ src/database/models/comment.entity.ts
@@ -0,0 +1,32 @@
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne, OneToMany, JoinColumn } from 'typeorm';
+import { User } from './user.entity';
+import { Post } from './post.entity';
+
+@Entity()
+export class Comment {
+  @PrimaryGeneratedColumn()
+  comment_id!: number;
+
+  @Column({ type: 'int', nullable: false })
+  post_id!: number;
+
+  @Column({ length: 45, nullable: false })
+  user_id!: string;
+
+  @Column({ length: 45, nullable: true, default: '내용 없음' })
+  comment_content!: string;
+
+  @Column({ type: 'tinyint', width: 1, default: 0 })
+  delete_flag!: number;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  created_at!: string;
+
+  @ManyToOne((type) => User)
+  @JoinColumn({ referencedColumnName: 'user_id' })
+  user!: User;
+
+  @ManyToOne((type) => Post)
+  @JoinColumn({ referencedColumnName: 'post_id' })
+  post!: Post;
+}

513954b80a1b5a4d9e30f4f1d489f0bdfba3e17c
subin <subin012358@gmail.com>
2023-05-12 02:02:22 +0900

    add: export files

--- src/database/daos/index.ts
+++ src/database/daos/index.ts
@@ -1,2 +1,7 @@
-export * from "./user.repo";
-export * from "./admin.repo";
+export * from './user.repo';
+export * from './admin.repo';
+export * from './post.repo';
+// export * from './comment.repo';
+export * from './pet.repo';
+
+// daos : 기본 CRUD 기능 폴더

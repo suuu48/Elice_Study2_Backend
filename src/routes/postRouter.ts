d0fd2449879e1087c7ac1764c80a0ee186101ffa
okonomiyakki <parkji12532@naver.com>
2023-05-16 08:00:58 +0900

    feat: Post 생성, 조회 라우터 추가

--- src/routes/postRouter.ts
+++ src/routes/postRouter.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
-import { addPost, getAllPosts } from '../services/postService';
+import { addPostHandler, getAllPostsHandler, getPostHandler } from '../controllers/postController';
 
 const router = Router();
 
-router.post('/write', addPost);
-
-router.get('/list', getAllPosts);
+router.post('/:user_id', addPostHandler);
+router.get('/', getAllPostsHandler);
+router.get('/:post_id', getPostHandler);
 
 export default router;

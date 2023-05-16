536d9f3c2d30a1eae3a2682cdf96afcc6f19be48
okonomiyakki <parkji12532@naver.com>
2023-05-17 02:16:43 +0900

    feat: Post 카테고리별 조회 라우터 생성

--- src/routes/postRouter.ts
+++ src/routes/postRouter.ts
@@ -1,10 +1,27 @@
 import { Router } from 'express';
-import { addPostHandler, getAllPostsHandler, getPostHandler } from '../controllers/postController';
+import {
+  addPostHandler,
+  getCategoriesHandler,
+  getAllPostsHandler,
+  getAllPostsByLocationHandler,
+  getPostHandler,
+} from '../controllers/postController';
 
 const router = Router();
 
+/* 게시글 생성 */
 router.post('/:user_id', addPostHandler);
-router.get('/', getAllPostsHandler);
+
+/* 게시글 카테고리 조회 */
+router.get('/categories', getCategoriesHandler);
+
+/* 게시글 목록 조회 */
+// router.get('/', getAllPostsHandler);
+
+/* 카테고리별 게시글 목록 조회 */
+router.get('/category/:post_category', getAllPostsByLocationHandler);
+
+/* post_id로 게시글 조회 */
 router.get('/:post_id', getPostHandler);
 
 export default router;

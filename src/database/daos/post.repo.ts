e6ac677c3d55e81d8213c751fd4c4a985b896b89
okonomiyakki <parkji12532@naver.com>
2023-05-17 02:15:15 +0900

    feat: Post 카테고리별 조회 Repo 추가

--- src/database/daos/post.repo.ts
+++ src/database/daos/post.repo.ts
@@ -27,7 +27,25 @@ const createPost = async (inputData: createPostInput): Promise<number> => {
   }
 };
 
-/* 전체 게시글 조회 */
+/* 게시글 카테고리 조회 */
+const findCategories = async (): Promise<string[]> => {
+  try {
+    const SQL = `
+    SELECT post_category
+    FROM post
+    GROUP BY post_category
+    `;
+
+    const [categoryRows]: any = await db.query(SQL);
+
+    return categoryRows;
+  } catch (error) {
+    console.log(error);
+    throw new AppError(500, '[ 쿼리 실행 에러 ]: 카테고리 조회 실패.');
+  }
+};
+
+/* 게시글 목록 조회 */
 const findPosts = async (): Promise<Post[]> => {
   try {
     const SQL = `
@@ -36,24 +54,65 @@ const findPosts = async (): Promise<Post[]> => {
     `;
 
     const [postRows]: any = await db.query(SQL);
-    if (!Array.isArray(postRows) || postRows.length === 0)
-      throw new Error('[ 전체 게시글 조회 오류 ]: 게시글 목록이 존재하지 않습니다.'); // App Error >> 서비스로 옮길 에정
+    // if (!Array.isArray(postRows) || postRows.length === 0)
+    //   throw new Error('[ 전체 게시글 조회 오류 ]: 게시글 목록이 존재하지 않습니다.');
 
     return postRows;
   } catch (error) {
     console.log(error);
-    throw new Error('[ 쿼리 실행 에러 ]: 게시글 전체 조회 실패.');
+    throw new Error('[ 쿼리 실행 에러 ]: 게시글 목록 조회 실패.');
   }
 };
 
-/* post_id로 특정 게시글 조회 */
+/* 카테고리별 게시글 목록 조회 */
+const findPostsByLocation = async (
+  user_location: string,
+  post_category: string
+): Promise<Post[]> => {
+  try {
+    const selectColums =
+      'post.post_id, post.user_id, user.user_nickname, post.post_title, post.post_img, COUNT(comment.post_id) AS comment_count, post.created_at';
+
+    const SQL = `
+    SELECT ${selectColums}
+      FROM post
+      JOIN user ON user.user_id = post.user_id
+      LEFT JOIN comment ON comment.post_id = post.post_id
+      WHERE user.user_location = ? AND post.post_category = ?
+      GROUP BY post.post_id, user.user_nickname
+    `;
+
+    const [postRows]: any = await db.query(SQL, [user_location, post_category]);
+
+    return postRows;
+  } catch (error) {
+    console.log(error);
+    throw new AppError(500, '[ 쿼리 실행 에러 ]: 카테고리별 게시글 목록 조회 실패.');
+  }
+};
+
+/* post_id로 게시글 조회 */
 const findPostById = async (post_id: number): Promise<Post> => {
   try {
+    const selectColums =
+      'post_id, user_img, user_nickname, post_title, post_content, COUNT(comment.post_id) AS comment_count';
+
     const SQL = `
-    SELECT * 
+    SELECT ${selectColums}
     FROM post 
+    JOIN user ON user.user_id = post.user_id
+    LEFT JOIN comment ON comment.post_id = post.post_id
     WHERE post_id = ?
     `;
+
+    // const selectColums2 = 'comment_id, user_nickname, comment_content, created_at' // comment 에서 작성 예정
+
+    // const SQL2 = `
+    // SELECT ${selectColums}
+    // FROM comment
+    // JOIN user ON user.user_id = comment.user_id
+    // WHERE post_id = ?
+    // `;
 
     const [post]: any = await db.query(SQL, [post_id]);
 
@@ -120,4 +179,12 @@ const softDeletePost = async (post_id: number): Promise<Post> => {
   }
 };
 
-export { createPost, findPosts, findPostById, updatePost, softDeletePost };
+export {
+  createPost,
+  findCategories,
+  findPosts,
+  findPostsByLocation,
+  findPostById,
+  updatePost,
+  softDeletePost,
+};

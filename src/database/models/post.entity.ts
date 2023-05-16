cabfac3e2dd34a5a4f0d8613cfc078d5e8da4bdb
okonomiyakki <parkji12532@naver.com>
2023-05-16 17:25:15 +0900

    fix: Post Entity 수정

--- src/database/models/post.entity.ts
+++ src/database/models/post.entity.ts
@@ -1,6 +1,15 @@
-import { Entity, Column, PrimaryGeneratedColumn, ManyToOne, OneToMany, JoinColumn } from 'typeorm';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  OneToMany,
+  JoinColumn,
+  Timestamp,
+} from 'typeorm';
 import { User } from './user.entity';
 import { Comment } from './comment.entity';
+
 interface PostProfile {
   user_id: string;
   post_category: string;
@@ -24,25 +33,25 @@ export class Post {
   @Column({ length: 45, nullable: false })
   post_category!: string;
 
-  @Column({ length: 45, nullable: true })
+  @Column({ length: 60, nullable: false })
   post_title!: string;
 
-  @Column({ length: 45, nullable: true })
+  @Column({ length: 250, nullable: false })
   post_content!: string;
 
-  @Column({ length: 45, nullable: true })
+  @Column({ length: 100, nullable: true, default: null })
   post_img!: string;
 
   @Column({ type: 'tinyint', width: 1, default: 0 })
   delete_flag!: boolean;
 
   @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
-  created_at!: Date;
+  created_at!: Timestamp;
 
   @ManyToOne((type) => User)
   @JoinColumn({ referencedColumnName: 'user_id' })
   user!: User;
 
   @OneToMany((type) => Comment, (comment) => comment.post)
-  comments!: Comment[]; // 댓글
+  comments!: Comment[];
 }

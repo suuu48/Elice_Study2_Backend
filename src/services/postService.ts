45993efa4b1c4024a573f588c9e053f4f726c7ed
okonomiyakki <parkji12532@naver.com>
2023-05-17 02:15:56 +0900

    feat: Post Service 카테고리별 조회 추가

--- src/services/postService.ts
+++ src/services/postService.ts
@@ -1,7 +1,13 @@
 import { Post } from '../database/models/post.entity';
 import { AppError } from '../utils/errorHandler';
 import { createPostInput, updatePostInput } from '../database/models/post.entity';
-import { createPost, findPosts, findPostById } from '../database/daos/post.repo';
+import {
+  createPost,
+  findCategories,
+  findPosts,
+  findPostsByLocation,
+  findPostById,
+} from '../database/daos/post.repo';
 
 /* 게시글 생성 */
 const addPost = async (inputData: createPostInput) => {
@@ -16,41 +22,81 @@ const addPost = async (inputData: createPostInput) => {
     if (error instanceof AppError) throw error;
     else {
       console.log(error);
-      throw new AppError(500, error.message || null);
+      throw new AppError(400, error.message || null);
     }
   }
 };
 
-/* 전체 게시글 조회 */
+/* 게시글 카테고리 조회 */
+const getCategories = async (): Promise<string[]> => {
+  try {
+    const foundCategories = await findCategories();
+
+    if (foundCategories.length === 0)
+      throw new Error('[ 카테고리 조회 에러 ] 존재하는 카테고리가 없습니다.');
+
+    return foundCategories;
+  } catch (error: any) {
+    if (error instanceof AppError) throw error;
+    else {
+      console.log(error);
+      throw new AppError(404, error.message);
+    }
+  }
+};
+
+/* 게시글 목록 조회 */
 const getAllPosts = async (): Promise<Post[]> => {
   try {
     const foundPosts = await findPosts();
 
-    if (foundPosts === undefined)
-      throw new Error('[ 전체 게시글 조회 에러 ] 존재하는 게시글이 없습니다.');
+    if (foundPosts.length === 0)
+      throw new Error('[ 게시글 목록 조회 에러 ] 존재하는 게시글이 없습니다.');
 
     return foundPosts;
   } catch (error: any) {
     if (error instanceof AppError) throw error;
     else {
       console.log(error);
-      throw new AppError(500, error.message);
+      throw new AppError(404, error.message);
     }
   }
 };
 
-/* post_id로 특정 게시글 조회 */
+/* 카테고리별 게시글 목록 조회 */
+const getAllPostsByLocation = async (
+  user_location: string,
+  post_category: string
+): Promise<Post[]> => {
+  try {
+    const foundPosts = await findPostsByLocation(user_location, post_category);
+
+    if (foundPosts.length === 0)
+      throw new Error('[ 카테고리별 게시글 목록 조회 에러 ] 존재하는 게시글이 없습니다.');
+
+    return foundPosts;
+  } catch (error: any) {
+    if (error instanceof AppError) throw error;
+    else {
+      console.log(error);
+      throw new AppError(404, error.message);
+    }
+  }
+};
+
+/* post_id로 게시글 조회 */
 const getPost = async (post_id: number): Promise<Post> => {
   try {
-    const post = await findPostById(post_id);
+    const foundPost = await findPostById(post_id);
 
-    if (post === undefined) throw new Error('[ 게시글 조회 에러 ] 게시글이 존재하지 않습니다.');
+    if (foundPost === undefined)
+      throw new Error('[ 게시글 조회 에러 ] 게시글이 존재하지 않습니다.');
 
-    return post;
+    return foundPost;
   } catch (error: any) {
     if (error instanceof AppError) throw error;
     else throw new AppError(404, error.message);
   }
 };
 
-export { addPost, getAllPosts, getPost };
+export { addPost, getCategories, getAllPosts, getAllPostsByLocation, getPost };

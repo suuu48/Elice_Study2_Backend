0f5b9b5c85f0dc81091c9aaeda262c0973197508
subin <subin012358@gmail.com>
2023-05-17 03:09:33 +0900

    add: user.router.ts

---
+++ src/routes/user.router.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response, NextFunction } from "express";
+//import * as user from "../services/user.service";
+export const userRoute = express();
+
+/*
+// 회원가입
+userRoute.get("/register", user.register );
+
+// 로그인
+userRoute.get("/logIn", user.logIn );
+
+// 유저정보 조회
+userRoute.get("/:userId", user.getUserInfo );
+*/

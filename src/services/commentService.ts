4ab7fc717a46e06bf6d52df2687c8bae72822a88
okonomiyakki <parkji12532@naver.com>
2023-05-17 22:45:46 +0900

    feat: 댓글 삭제 기능 추가 in commentService.ts

--- src/services/commentService.ts
+++ src/services/commentService.ts
@@ -1,6 +1,6 @@
 import { AppError } from '../utils/errorHandler';
 import { createCommentInput, updateCommentInput } from '../database/models/comment.entity';
-import { findCommentById, createComment } from '../database/daos/comment.repo';
+import { findCommentById, createComment, deleteComment } from '../database/daos/comment.repo';
 
 /* 댓글 등록 */
 const addComment = async (inputData: createCommentInput) => {
@@ -19,4 +19,19 @@ const addComment = async (inputData: createCommentInput) => {
   }
 };
 
-export { addComment };
+/* 댓글 삭제 */
+const removeComment = async (comment_id: number) => {
+  try {
+    const foundDeletedCommentId = await deleteComment(comment_id);
+
+    return foundDeletedCommentId;
+  } catch (error: any) {
+    if (error instanceof AppError) throw error;
+    else {
+      console.log(error);
+      throw new AppError(404, error.message);
+    }
+  }
+};
+
+export { addComment, removeComment };

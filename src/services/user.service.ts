5193f881d04f32400217dfad3189eafa552f627c
subin <subin012358@gmail.com>
2023-05-18 16:16:11 +0900

    feat: service 작성

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,113 +1,106 @@
-import express, { Request, Response, NextFunction } from 'express';
-import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import * as userRepo from '../database/daos/user.repo';
+import { createUserInput, updateUserInput, User, UserProfile } from '../database/models';
+import { env } from '../config/envconfig';
 
 // 회원가임
-export const register = async (req: Request, res: Response, next: NextFunction) => {
+export const register = async (inputData: createUserInput) => {
+  // Todo : register >> createUser로 변경
   try {
-    const { userId, password, userName, userNickname, locationUser, userImg } = req.body;
+    const newUser = await userRepo.createUser(inputData);
 
-    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await userRepo.findOne(newUser.user_id);
+    if (!user) throw new Error('[ 유저 가입 에러 ] 가입된 유저를 찾을 수 없습니다.');
 
-    const data = {
-      userId,
-      password: hashedPassword,
-      userName,
-      userNickname,
-      locationUser,
-      userImg,
-    };
-
-    // 아이디 중복체크
-    const foundId = await userRepo.findOne(userId);
-    if (foundId) {
-      throw Error('이 아이디는 현재 사용중입니다. 다른 아이디를 입력해 주세요.');
-    }
-
-    // 닉네임 중복체크
-    const checkNickname = await userRepo.findOneByNickname(userId);
-    if (checkNickname) {
-      throw Error('이 닉네임은 현재 사용중입니다. 다른 닉네임을 입력해 주세요.');
-    }
-
-    const newUser = await userRepo.createUser(data);
-
-    res.status(200).json(newUser);
-  } catch (error) {
-    console.error(error);
+    return user;
+  } catch (error: any) {
+    //if (error instanceof AppError) throw error;
+    // else {
+    console.log(error);
+    // throw new AppError(500, error.message || null);
+    // }
   }
 };
 
 // 로그인
-export const logIn = async (req: Request, res: Response, next: NextFunction) => {
+export const getUserToken = async (
+  userId: string
+): Promise<{ user: User; accessToken: string; refreshToken: string }> => {
   try {
-    const { userId, password } = req.body;
-    if (!userId) {
-      console.log('아이디를 입력해주세요.');
-      //return next(new AppError(400, '아이디를 입력해주세요.'));
-    }
-    if (!password) {
-      console.log('비밀번호를 입력해주세요.');
-      // return next(new AppError(400, '비밀번호를 입력해주세요.'));
-    }
-
-    const foundUser = await userRepo.findAllInfo(userId);
-    if (!foundUser) console.log('존재하지 않는 아이디 입니다.');
-
-    const correctPasswordHash = foundUser.user_password; // db에 저장되어 있는 암호화된 비밀번호
-
-    // 매개변수의 순서 중요 (1번째는 프론트가 보내온 비밀번호, 2번쨰는 db에 있떤 암호화된 비밀번호)
-    const isPasswordCorrect = await bcrypt.compare(password, correctPasswordHash);
-
-    if (!isPasswordCorrect) {
-      throw new Error('비밀번호가 일치하지 않습니다. 다시 한 번 확인해 주세요.');
-    }
+    const user = await userRepo.findInfo(userId);
     // 로그인 성공 -> JWT 웹 토큰 생성
-    const secretKey = process.env.JWT_SECRET_KEY || 'secret-key';
+    // const secretKey = env.JWT_SECRET_KEY || 'secret-key';
+    const accessTokenSecret = env.ACCESS_TOKEN_SECRET || 'default-access-token-secret';
+    const refreshTokenSecret = env.REFRESH_TOKEN_SECRET || 'default-refresh-token-secret';
 
-    const accessToken = jwt.sign(
-      { id: foundUser.user_id, password: foundUser.user_password, verify: foundUser.verify },
-      secretKey
-    );
+    const payload = {
+      userId: user.user_id,
+      password: user.user_password,
+      verify: user.verify,
+      location: user.user_location,
+    };
 
-    res.status(200).json({
-      message: '로그인 성공',
-      data: {
-        userId: foundUser.user_id,
-        userName: foundUser.user_name,
-        userNickname: foundUser.user_nickname,
-        userLocation: foundUser.user_location,
-        userImg: foundUser.user_img,
-        accessToken,
-        // refreshToken,
-      },
+    const accessToken = jwt.sign(payload, accessTokenSecret, {
+      expiresIn: env.ACCESS_TOKEN_EXPIRES_IN,
     });
+
+    const refreshToken = jwt.sign(payload, refreshTokenSecret, {
+      expiresIn: env.REFRESH_TOKEN_EXPIRES_IN,
+    });
+
+    return { user, accessToken, refreshToken };
   } catch (error) {
     console.error(error);
+    throw new Error('[JWT 토큰 에러] 토큰 발급에 실패했습니다.');
   }
 };
 
 // 유저 정보 조회
-export const getUserInfo = async (req: Request, res: Response, next: NextFunction) => {
+export const getUser = async (userId: string): Promise<UserProfile> => {
   try {
-    const { userId } = req.body;
-    if (!userId) {
-      console.log('아이디를 입력해주세요.');
-      //return next(new AppError(400, '아이디를 입력해주세요.'));
-    }
-
     const foundUser = await userRepo.findOne(userId);
-    if (!foundUser) console.log('존재하지 않는 아이디 입니다.'); //Todo: repo에서 에러처리하기
+    if (!foundUser) console.log('존재하지 않는 아이디 입니다.');
     //return next(new AppError(400, '존재하지 않는 아이디입니다.'));
 
-    res.status(200).json({
-      message: '회원정보',
-      data: foundUser,
-    });
-    // res.status(200).json(foundUser); // Todo: 어떻게 반환할지 결정하기
+    return foundUser;
   } catch (error) {
     console.error(error);
-    next(error);
+    throw new Error('[유저 정보 조회 에러] 정보 조회에 실패했습니다.');
+    //  next(error);
+  }
+};
+
+// 유저 정보 수정
+export const updateUserInfo = async (
+  userId: string,
+  updateData: updateUserInput
+): Promise<User> => {
+  try {
+    const foundUser = await userRepo.findOne(userId);
+    if (!foundUser) console.log('존재하지 않는 아이디 입니다.');
+    //return next(new AppError(400, '존재하지 않는 아이디입니다.'));
+
+    const updateUser = await userRepo.updateUser(userId, updateData);
+    return updateUser;
+  } catch (error) {
+    console.error(error);
+    throw new Error('[유저 수정 에러] 유저 정보 수정에 실패했습니다.');
+    //  next(error);
+  }
+};
+
+// 유저 소프트 삭제
+export const softDelete = async (userId: string): Promise<User> => {
+  try {
+    const foundUser = await userRepo.findOne(userId);
+    if (!foundUser) console.log('존재하지 않는 아이디 입니다.');
+
+    const deleteUser = await userRepo.softDeleteUser(userId);
+    return deleteUser;
+  } catch (error: any) {
+    console.log(error);
+    throw new Error('유저 삭제에 실패했습니다.'); // Todo: if..else 문으로 변경하기
+    // if (error instanceof AppError) throw error;
+    // else throw new AppError(404, error.message);
   }
 };

08fff81391641f57a1f4bb50578964b1dd7be6e2
subin <subin012358@gmail.com>
2023-05-18 16:10:04 +0900

    update: ACCESS_TOKEN, REFRESH_TOKEN 추가

--- src/config/envconfig.ts
+++ src/config/envconfig.ts
@@ -3,15 +3,17 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const env = {
-  BCRYPT_SALT_ROUNDS: process.env.BCRYPT_SALT_ROUNDS,
+  // BCRYPT_SALT_ROUNDS: process.env.BCRYPT_SALT_ROUNDS,
   PORT: process.env.PORT,
   DB_HOST: process.env.DB_HOST,
   DB_PORT: process.env.DB_PORT,
   DB_USERNAME: process.env.DB_USERNAME,
   DB_PASSWORD: process.env.DB_PASSWORD,
   DB_DBNAME: process.env.DB_DBNAME,
-  // ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
-  // REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET,
-  // ACCESS_TOKEN_EXPIRES_IN: process.env.ACCESS_TOKEN_EXPIRES_IN,
-  // REFRESH_TOKEN_EXPIRES_IN: process.env.REFRESH_TOKEN_EXPIRES_IN,
+
+  JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
+  ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
+  REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET,
+  ACCESS_TOKEN_EXPIRES_IN: process.env.ACCESS_TOKEN_EXPIRES_IN,
+  REFRESH_TOKEN_EXPIRES_IN: process.env.REFRESH_TOKEN_EXPIRES_IN,
 };

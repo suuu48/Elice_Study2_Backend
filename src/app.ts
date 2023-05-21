6e45cdae222edf351b3e67b1baf1c1d8d37fa6e7
okonomiyakki <parkji12532@naver.com>
2023-05-22 02:46:58 +0900

    add: 라우터 코드컨벤션 통일하기

--- src/app.ts
+++ src/app.ts
@@ -2,25 +2,10 @@ import express, { Request, Response } from 'express';
 import { env } from './config/envconfig';
 import { db } from './config/dbconfig';
 import { errorHandlerMiddleware } from './utils/errorHandler';
-import { v1Router, postRouter, commentRouter } from './routes';
-// import { postRouter, commentRouter } from './routes/index';
+import { v1Router } from './routes';
 
 const port = Number(env.PORT || 3000);
 const app = express();
-
-interface test {
-  title: string;
-  content: string;
-}
-
-const test: test = {
-  title: 'peeps',
-  content: 'typescript-server',
-};
-
-app.get('/', (req: Request, res: Response) => {
-  res.send(test);
-});
 
 db.getConnection()
   .then(async () => {
@@ -43,10 +28,6 @@ db.getConnection()
   .catch((err) => console.log('error!!!!!!!', err));
 
 app.use(express.json());
-app.use('/post', postRouter);
-app.use('/comment', commentRouter);
-// app.use('/user', -)
-// app.use('/pet', -)
-// app.use('/review', -)
+
 app.use('/api/v1', v1Router);
 app.use(errorHandlerMiddleware);
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,16 +1,21 @@
 import express from 'express';
 
+import rootRouter from './rootRouter';
 import { userRoute } from './user.router';
 import { reviewRoute } from './review.router';
 import { adminRoute } from './admin.router';
 import { petRoute } from './pet.router';
 import postRouter from './postRouter';
 import commentRouter from './commentRouter';
+
 const v1Router = express.Router();
 
+v1Router.use('/', rootRouter);
 v1Router.use('/user', userRoute);
 v1Router.use('/review', reviewRoute);
 v1Router.use('/admin', adminRoute);
 v1Router.use('/pet', petRoute);
+v1Router.use('/post', postRouter);
+v1Router.use('/comment', commentRouter);
 
-export { postRouter, commentRouter, v1Router };
+export { v1Router };

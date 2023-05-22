0f147fbc0540b263f1a1a0afb545f9207bee1d59
subin <subin012358@gmail.com>
2023-05-22 22:53:21 +0900

    update: 유저 전체 조회 & 토큰으로 admin 확인

--- src/controllers/adminController.ts
+++ src/controllers/adminController.ts
@@ -1,8 +1,12 @@
 import { Request, Response, NextFunction } from 'express';
 import * as adminService from '../services/admin.service';
+import { AppError } from '../utils/errorHandler';
 
-// 유저 정보 조회
-export const deleteFindUserHandler = async (req: Request, res: Response, next: NextFunction) => {
+// 삭제할 유저 정보 조회
+export const FindALlHandler = async (req: Request, res: Response, next: NextFunction) => {
+  if(req.body.verify ==='user') throw new AppError(400, '관리자가 아닙니다.');
+
+
   try {
     const { adminId } = req.params;
 
@@ -10,7 +14,7 @@ export const deleteFindUserHandler = async (req: Request, res: Response, next: N
 
     const usersInfo = await adminService.getUsersInfo();
 
-    res.status(201).json({ message: '유저 로그인 성공', data: usersInfo });
+    res.status(201).json({ message: '유저 전체 조회 성공', data: usersInfo });
   } catch (error: any) {
     //if (error instanceof AppError) next(error);
     //else {
@@ -23,6 +27,7 @@ export const deleteFindUserHandler = async (req: Request, res: Response, next: N
 // 유저 정보 하드 삭제
 // 리뷰 삭제
 export const deleteUserHandler = async (req: Request, res: Response, next: NextFunction) => {
+  if(req.body.verify ==='user') throw new AppError(400, '관리자가 아닙니다.');
   try {
     const { userId } = req.params;
     if (!userId) throw new Error('[ 요청 에러 ] 아이디를 반드시 입력해야 합니다.');
@@ -39,6 +44,7 @@ export const deleteUserHandler = async (req: Request, res: Response, next: NextF
 
 // 유저 계정 복구
 export const restoreUserHandler = async (req: Request, res: Response, next: NextFunction) => {
+  if(req.body.verify ==='user') throw new AppError(400, '관리자가 아닙니다.');
   try {
     const { userId } = req.params;
     if (!userId) throw new Error('[ 요청 에러 ] 아이디를 반드시 입력해야 합니다.');

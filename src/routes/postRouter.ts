b0a93b5ba118d2b797a111ea46efa6c28b8bdd16
okonomiyakki <parkji12532@naver.com>
2023-05-23 09:59:59 +0900

    update: API 반환 타입 추가 in postRouter.ts

--- src/routes/postRouter.ts
+++ src/routes/postRouter.ts
@@ -1,6 +1,13 @@
 import { Router } from 'express';
 import processImage from '../middlewares/multer';
-import { getPostsOutput } from '../database/models/post.entity';
+import {
+  foundAllPostOutput,
+  foundCategoriesOutput,
+  foundPostsOutput,
+  foundPostOutput,
+  createdPostOutput,
+  updatedPostOutput,
+} from '../database/models/post.entity';
 import {
   getAllPostsHandler,
   getCategoriesHandler,
@@ -15,25 +22,25 @@ import {
 const postRouter = Router();
 
 /* 게시글 목록 조회 */
-postRouter.get('/', getAllPostsHandler);
+postRouter.get('/', getAllPostsHandler<foundAllPostOutput>);
 
 /* 게시글 카테고리 조회 */
-postRouter.get('/categories', getCategoriesHandler);
+postRouter.get('/categories', getCategoriesHandler<foundCategoriesOutput>);
 
 /* 키워드별 게시글 목록 조회 */
-postRouter.get('/search', getSearchedPostsByKeywordHandler<getPostsOutput>);
+postRouter.get('/search', getSearchedPostsByKeywordHandler<foundPostsOutput>);
 
 /* 카테고리별 게시글 목록 조회 */
-postRouter.get('/category/:post_category', getAllPostsByLocationHandler<getPostsOutput>);
+postRouter.get('/category/:post_category', getAllPostsByLocationHandler<foundPostsOutput>);
 
 /* 게시글 및 게시글별 댓글 목록 조회 */
-postRouter.get('/:post_id', getPostHandler);
+postRouter.get('/:post_id', getPostHandler<foundPostOutput>);
 
 /* 게시글 등록 */
-postRouter.post('/:user_id', processImage, addPostHandler);
+postRouter.post('/:user_id', processImage, addPostHandler<createdPostOutput>);
 
 /* 게시글 수정 */
-postRouter.patch('/:post_id', processImage, editPostHandler);
+postRouter.patch('/:post_id', processImage, editPostHandler<updatedPostOutput>);
 
 /* 게시글 삭제 */
 postRouter.delete('/:post_id', removePostHandler);

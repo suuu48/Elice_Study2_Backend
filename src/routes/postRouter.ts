82e4624c6731e9ce0b14efdeb5df8021711e2c4e
okonomiyakki <parkji12532@naver.com>
2023-05-23 21:25:24 +0900

    add: jwt 미들웨어 연결 in postRouter.ts

--- src/routes/postRouter.ts
+++ src/routes/postRouter.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { isAccessTokenValid } from '../middlewares';
 import processImage from '../middlewares/multer';
 import {
   foundAllPostOutput,
@@ -22,27 +23,31 @@ import {
 const postRouter = Router();
 
 /* 게시글 목록 조회 */
-postRouter.get('/', getAllPostsHandler<foundAllPostOutput>);
+postRouter.get('/', isAccessTokenValid, getAllPostsHandler<foundAllPostOutput>);
 
 /* 게시글 카테고리 조회 */
-postRouter.get('/categories', getCategoriesHandler<foundCategoriesOutput>);
+postRouter.get('/categories', isAccessTokenValid, getCategoriesHandler<foundCategoriesOutput>);
 
 /* 키워드별 게시글 목록 조회 */
-postRouter.get('/search', getSearchedPostsByKeywordHandler<foundPostsOutput>);
+postRouter.get('/search', isAccessTokenValid, getSearchedPostsByKeywordHandler<foundPostsOutput>);
 
 /* 카테고리별 게시글 목록 조회 */
-postRouter.get('/category/:post_category', getAllPostsByLocationHandler<foundPostsOutput>);
+postRouter.get(
+  '/category/:post_category',
+  isAccessTokenValid,
+  getAllPostsByLocationHandler<foundPostsOutput>
+);
 
 /* 게시글 및 게시글별 댓글 목록 조회 */
-postRouter.get('/:post_id', getPostHandler<foundPostOutput>);
+postRouter.get('/:post_id', isAccessTokenValid, getPostHandler<foundPostOutput>);
 
 /* 게시글 등록 */
-postRouter.post('/:user_id', processImage, addPostHandler<createdPostOutput>);
+postRouter.post('/:user_id', isAccessTokenValid, processImage, addPostHandler<createdPostOutput>);
 
 /* 게시글 수정 */
-postRouter.patch('/:post_id', processImage, editPostHandler<updatedPostOutput>);
+postRouter.patch('/:post_id', isAccessTokenValid, processImage, editPostHandler<updatedPostOutput>);
 
 /* 게시글 삭제 */
-postRouter.delete('/:post_id', removePostHandler);
+postRouter.delete('/:post_id', isAccessTokenValid, removePostHandler);
 
 export default postRouter;

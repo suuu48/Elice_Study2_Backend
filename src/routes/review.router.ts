c6ed88d03278eb567a0884ad8bc62649f4bcc8e0
subin <subin012358@gmail.com>
2023-05-23 16:26:01 +0900

    feat: img 미들웨어 적용

--- src/routes/review.router.ts
+++ src/routes/review.router.ts
@@ -1,20 +1,21 @@
 import express from 'express';
 import * as reviewController from '../controllers/reviewController';
 import { isAccessTokenValid } from '../middlewares';
+import processImage from '../middlewares/multer';
 
 export const reviewRoute = express();
 
 // 리뷰 등록
-reviewRoute.post('/', isAccessTokenValid, reviewController.addReviewHandler);
+reviewRoute.post('/', isAccessTokenValid, processImage, reviewController.addReviewHandler);
 
 // 리뷰 상세 조회
-reviewRoute.get('/:review_id',isAccessTokenValid,  reviewController.getReviewHandler);
+reviewRoute.get('/:review_id', isAccessTokenValid, reviewController.getReviewHandler);
 
 // 장소별 리뷰 전체 조회
-reviewRoute.get('/',isAccessTokenValid,  reviewController.getAllReviewsHandler);
+reviewRoute.get('/', isAccessTokenValid, reviewController.getAllReviewsHandler);
 
 // 리뷰 수정
-reviewRoute.post('/:review_id', isAccessTokenValid, reviewController.updateReviewHandler);
+reviewRoute.post('/:review_id', isAccessTokenValid, processImage, reviewController.updateReviewHandler);
 
 // 리뷰 삭제
-reviewRoute.delete('/:review_id',isAccessTokenValid,  reviewController.deleteReviewHandler);
+reviewRoute.delete('/:review_id', isAccessTokenValid, reviewController.deleteReviewHandler);

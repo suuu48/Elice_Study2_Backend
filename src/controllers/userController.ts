4d6315e5cd2aaf48acf656d214ee0d2b9061336b
subin <subin012358@gmail.com>
2023-05-23 16:27:38 +0900

    feat: img 업로드,삭제 기능 추가

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -3,22 +3,14 @@ import * as userService from '../services/user.service';
 import * as User from '../database/models';
 import bcrypt from 'bcrypt';
 import * as userRepo from '../database/daos/user.repo';
-import { createUser } from '../services/user.service';
 import { AppError } from '../utils/errorHandler';
 
 // 유저 추가
 export const addUserHandler = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const { user_id, user_name, user_password, user_nickname, user_location, user_img } = req.body;
-
-    if (
-      !user_id ||
-      !user_name ||
-      !user_password ||
-      !user_nickname ||
-      !user_location ||
-      !user_img
-    )
+    const { user_id, user_name, user_password, user_nickname, user_location } = req.body;
+    const imgFileRoot = `http://localhost:3000/api/v1/static/${req.file?.filename}`;
+    if (!user_id || !user_name || !user_password || !user_nickname || !user_location)
       throw new Error('[ 요청 에러 ] 사진을 제외한 모든 필드를 입력해야 합니다.');
 
     const isCheckId = await userRepo.findOne(user_id);
@@ -40,7 +32,7 @@ export const addUserHandler = async (req: Request, res: Response, next: NextFunc
       user_password: hashedPassword,
       user_nickname,
       user_location,
-      user_img,
+      user_img: imgFileRoot,
     };
 
     const createdUser = await userService.createUser(userData);
@@ -64,25 +56,24 @@ export const logIn = async (req: Request, res: Response, next: NextFunction) => 
     if (!user_id || !user_password)
       throw new Error('[ 요청 에러 ] 아이디와 비밀번호를 반드시 입력해야 합니다.');
 
-    const checkId = await userRepo.findInfo(user_id);
+    const checkId = await userRepo.findAllInfo(user_id);
     if (!checkId) {
       throw Error('해당 아이디는 가입 내역이 없습니다. 다른 아이디를 입력해 주세요.');
     }
 
     const correctPasswordHash = checkId.user_password; // db에 저장되어 있는 암호화된 비밀번호
 
-    if(correctPasswordHash !== user_password){
-     const isPasswordCorrect = await bcrypt.compare(user_password, correctPasswordHash);
+    if (correctPasswordHash !== user_password) {
+      const isPasswordCorrect = await bcrypt.compare(user_password, correctPasswordHash);
       if (!isPasswordCorrect) {
         throw new Error('비밀번호가 일치하지 않습니다. 다시 한 번 확인해 주세요.');
       }
     }
 
-
     const userToken = await userService.getUserToken(checkId.user_id);
 
     res.status(201).json({
-      message: '유저 로그인 성공',
+      message: '로그인 성공',
       data: {
         userToken,
       },
@@ -93,7 +84,7 @@ export const logIn = async (req: Request, res: Response, next: NextFunction) => 
       next(error);
     } else {
       console.log(error);
-      throw new AppError(500, '[ HTTP 요청 에러 ] 유저 로그인 실패');
+      throw new AppError(500, '[ HTTP 요청 에러 ] 로그인 실패');
     }
   }
 };
@@ -123,19 +114,20 @@ export const getUserInfo = async (req: Request, res: Response, next: NextFunctio
 export const updateUserHandler = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { userId } = req.params;
+    const imgFileRoot = `http://localhost:3000/api/v1/static/${req.file?.filename}`;
 
     if (!userId) throw new Error('[ 요청 에러 ] 아이디를 반드시 입력해야 합니다.');
 
-    const { user_name, user_nickname, user_location, user_img } = req.body;
+    const { user_name, user_nickname, user_location } = req.body;
 
-    if (!user_name && !user_nickname && !user_location && !user_img)
+    if (!user_name && !user_nickname && !user_location)
       throw new Error('[ 요청 에러 ] 변경된 값이 없습니다!');
 
     const updateUserData: User.updateUserInput = {
       user_name,
       user_nickname,
       user_location,
-      user_img,
+      user_img: imgFileRoot,
     };
 
     const userInfo = await userService.updateUserInfo(userId, updateUserData);
@@ -159,8 +151,8 @@ export const softDeleteUserHandler = async (req: Request, res: Response, next: N
     if (!userId) throw new Error('[ 요청 에러 ] 아이디를 반드시 입력해야 합니다.');
 
     const user = await userService.softDelete(userId);
-
-    res.status(200).json({ message: '유저 소프트 삭제 성공', data: user });
+    const returnUserId = user.user_id;
+    res.status(200).json({ message: '유저 소프트 삭제 성공', data: returnUserId });
   } catch (error: any) {
     if (error instanceof AppError) {
       if (error.statusCode === 400) console.log(error);

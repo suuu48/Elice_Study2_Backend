f625aef76387bb60711a4cb4f18432069d41b014
okonomiyakki <parkji12532@naver.com>
2023-05-25 03:13:50 +0900

    update: user, review 더미데이터 수정

--- src/database/dummyDatas.ts
+++ src/database/dummyDatas.ts
@@ -1,35 +1,42 @@
 import { db } from '../config/dbconfig';
 
-// const userQuery = `INSERT INTO user (user_id, user_name, user_password, user_nickname, verify, location_user, delete_flag, user_img) VALUES
-// ('user13', '테스트', 'pwd1234', '테스트닉네임', 'user', '강서구', 0, NULL)`;
+// const userQuery = `INSERT INTO user (user_id, user_name, user_password, user_nickname, verify, user_location, user_img, delete_flag, deleted_at) VALUES
+// ('user13', '테스트', 'pwd1234', '테스트닉네임', 'user', '서울시 성동구', 0, DEFAULT, 0, DEFAULT)`;
 
-// const userQuery = `INSERT INTO user (user_id, user_name, user_password, user_nickname, verify, location_user, delete_flag, user_img) VALUES
-// ('admin1', '관리자1', 'pwd4321', '운영자1', 'admin', '용산구', 0, NULL),
-// ('admin2', '관리자2', 'pwd4321', '운영자2', 'admin', '중구', 0, NULL),
-// ('user1', '김민철', 'pwd1234', '민철', 'user', '마포구', 0, NULL),
-// ('user2', '이승연', 'pwd1234', '승연', 'user', '마포구', 0, NULL),
-// ('user3', '박민지', 'pwd1234', '민지', 'user', '마포구', 0, NULL),
-// ('user4', '최민수', 'pwd1234', '민수', 'user', '서초구', 0, NULL),
-// ('user5', '한영호', 'pwd1234', '영호', 'user', '서초구', 0, NULL),
-// ('user6', '장지원', 'pwd1234', '지원', 'user', '서초구', 0, NULL),
-// ('user7', '윤지은', 'pwd1234', '지은', 'user', '종로구', 0, NULL),
-// ('user8', '성유진', 'pwd1234', '유진', 'user', '종로구', 0, NULL),
-// ('user9', '양은지', 'pwd1234', '은지', 'user', '종로구', 0, NULL),
-// ('user10', '유준형', 'pwd1234', '준형', 'user', '성동구', 0, NULL),
-// ('user11', '임지훈', 'pwd1234', '지훈', 'user', '성동구', 0, NULL),
-// ('user12', '송기훈', 'pwd1234', '기훈', 'user', '성동구', 0, NULL);`;
+// const userQuery = `INSERT INTO user (user_id, user_name, user_password, user_nickname, verify, user_location, user_img, delete_flag, deleted_at) VALUES
+// ('admin1', '관리자1', 'pwd4321', '운영자1', 'admin', '서울시 성동구', DEFAULT, 0, DEFAULT),
+// ('admin2', '관리자2', 'pwd4321', '운영자2', 'admin', '서울시 성동구', DEFAULT, 0, DEFAULT),
+// ('user1', '김민철', 'pwd1234', '민철', 'user', '서울시 성동구', DEFAULT, 0, DEFAULT),
+// ('user2', '이승연', 'pwd1234', '승연', 'user', '서울시 성동구', DEFAULT, 0, DEFAULT),
+// ('user3', '박민지', 'pwd1234', '민지', 'user', '서울시 성동구', DEFAULT, 0, DEFAULT),
+// ('user4', '최민수', 'pwd1234', '민수', 'user', '서울시 성동구', DEFAULT, 0, DEFAULT),
+// ('user5', '한영호', 'pwd1234', '영호', 'user', '서울시 성동구', DEFAULT, 0, DEFAULT),
+// ('user6', '장지원', 'pwd1234', '지원', 'user', '서울시 성동구', DEFAULT, 0, DEFAULT),
+// ('user7', '윤지은', 'pwd1234', '지은', 'user', '서울시 성동구', DEFAULT, 0, DEFAULT),
+// ('user8', '성유진', 'pwd1234', '유진', 'user', '서울시 성동구', DEFAULT, 0, DEFAULT),
+// ('user9', '양은지', 'pwd1234', '은지', 'user', '서울시 성동구', DEFAULT, 0, DEFAULT),
+// ('user10', '유준형', 'pwd1234', '준형', 'user', '서울시 성동구', DEFAULT, 0, DEFAULT),
+// ('user11', '임지훈', 'pwd1234', '지훈', 'user', '서울시 성동구', DEFAULT, 0, DEFAULT),
+// ('user12', '송기훈', 'pwd1234', '기훈', 'user', '서울시 성동구', DEFAULT, 0, DEFAULT);`;
 
-const reviewQuery = `INSERT INTO review (review_id, location_id, location_name, location_category, user_id, review_content, star_rating, created_at) VALUES 
-(DEFAULT, '1234', '강아지나라', '병원', 'user1', '진료가 친절해서 좋았습니다.', 4.3, DEFAULT),
-(DEFAULT, '5678', '푸들하우스', '미용실', 'user10', '강아지 털이 깔끔하게 잘라줬어요!', 4, DEFAULT),
-(DEFAULT, '2468', '멍멍애견용품', '애견용품점', 'user11', '다양한 애견용품이 있어서 좋았어요!', 4.5, DEFAULT),
-(DEFAULT, '1357', '댕댕스테이션', '카페', 'user12', '강아지랑 함께 커피한잔 하기에 딱 좋은 곳이에요!', 4, DEFAULT),
-(DEFAULT, '3691', '우리강아지공원', '공원', 'user2', '강아지와 함께 산책하기 좋은 장소입니다!', 4.5, DEFAULT),
-(DEFAULT, '7894', '애니멀병원', '병원', 'user3', '진료하는 동물병원 중에 가격이 저렴한 곳입니다.', 3.5, DEFAULT),
-(DEFAULT, '9512', '왈왈애견미용실', '미용실', 'user4', '토이푸들 털이 예뻐서 다음에도 방문할 예정입니다!', 4.7, DEFAULT),
-(DEFAULT, '7539', '행복한강아지', '애견용품점', 'user5', '애견음식이 너무 풍부합니다.', 4.2, DEFAULT),
-(DEFAULT, '6541', '좋은애견카페', '카페', 'user6', '펫플레이트를 먹어보았는데, 강아지가 정말 좋아했습니다.', 4.5, DEFAULT),
-(DEFAULT, '7953', '멍멍놀이터', '공원', 'user7', '좋은 날씨에 강아지와 산책하기에 딱 좋은 공원입니다.', 4.2, DEFAULT);`;
+const reviewQuery = `INSERT INTO review (review_id, location_id,  user_id, review_content, star_rating, created_at, review_img) VALUES 
+(DEFAULT, '1953249219', 'user1', '진료가 너무 최악이었어요.', 1, DEFAULT, DEFAULT),
+(DEFAULT, '1953249219', 'user2', '진료가 별로였어요.', 2, DEFAULT, DEFAULT),
+(DEFAULT, '1953249219', 'user3', '진료가 그냥 그랬어요.', 3, DEFAULT, DEFAULT),
+(DEFAULT, '1953249219', 'user4', '진료가 마음에 들었어요.', 4, DEFAULT, DEFAULT),
+(DEFAULT, '1953249219', 'user5', '진료가 최고였어요!', 5, DEFAULT, DEFAULT),
+(DEFAULT, '1953249219', 'user5', '의사선생님이 친절해요!', 5, DEFAULT, DEFAULT),
+(DEFAULT, '1953249219', 'user5', '병원비가 저렴해요!', 5, DEFAULT, DEFAULT),
+(DEFAULT, '1953249219', 'user5', '강아지가 좋아했어요!', 5, DEFAULT, DEFAULT),
+(DEFAULT, '5678', 'user10', '강아지 털이 깔끔하게 잘라줬어요!', 4, DEFAULT, DEFAULT),
+(DEFAULT, '2468', 'user11', '다양한 애견용품이 있어서 좋았어요!', 5, DEFAULT, DEFAULT),
+(DEFAULT, '1357', 'user12', '강아지랑 함께 커피한잔 하기에 딱 좋은 곳이에요!', 4, DEFAULT, DEFAULT),
+(DEFAULT, '3691', 'user2', '강아지와 함께 산책하기 좋은 장소입니다!', 4, DEFAULT, DEFAULT),
+(DEFAULT, '7894', 'user3', '진료하는 동물병원 중에 가격이 저렴한 곳입니다.', 3, DEFAULT, DEFAULT),
+(DEFAULT, '9512', 'user4', '토이푸들 털이 예뻐서 다음에도 방문할 예정입니다!', 4, DEFAULT, DEFAULT),
+(DEFAULT, '7539', 'user5', '애견음식이 너무 진부합니다.', 2, DEFAULT, DEFAULT),
+(DEFAULT, '6541', 'user6', '펫플레이트를 먹어보았는데, 강아지가 정말 좋아했습니다.', 3, DEFAULT, DEFAULT),
+(DEFAULT, '7953', 'user7', '좋은 날씨에 강아지와 산책하기에 딱 좋은 공원입니다.', 5, DEFAULT, DEFAULT);`;
 
 const postQuery = `INSERT INTO post (post_id, user_id, post_category, post_title, post_content, post_img, comment_id, delete_flag, created_at) VALUES
 (DEFAULT, 'user1', '일상', '반려견과 함께하는 달콤한 일상', '반려견과 함께하는 달콤한 일상을 즐겨봅시다.', null, null, 0, DEFAULT),
